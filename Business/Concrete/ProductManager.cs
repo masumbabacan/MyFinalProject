23ff008bb0d7b2de8cc8582c1566646e0f455811
MUHAMMED MASUM BABACAN <201910614033@ogr.balikesir.edu.tr>
2021-03-18 15:45:28 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            this._productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+
+            //İŞ KODLARI
+
+            return _productDal.GetAll();
+
+            
+        }
+    }
+}
